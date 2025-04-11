c8e096503767bf73aa3a7a7890c6aa62b9245a6d
Pedro Henrique <pedroh.de.jesus12@gmail.com>
2025-04-11 00:35:13 -0300

    feat: redireciona usuário para tela de login ao acessar root

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 console.log('Arquivo index.js foi carregado');
+
+//Pega os frameworks do Node.js a serem usados
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
+// Middleware para liberar e mexer nos arquivos da pasta public pro front-end
+app.use(express.static(path.join(__dirname, '/public')));//_dirname é a localização do arquivo atual
+
+//Abre o arquivo de login ao acessar o root
 app.get('/', (req, res) => {
-  res.send('Servidor Express funcionando!');
+  res.sendFile(path.join(__dirname, '/public/login.html'));
 });
 
+//Exibe log quando iniciar o servidor
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000');
 });
